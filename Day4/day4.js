c7705997a5721d4fed6657a1b2c8c22765a51e62
mohd saif <msaifansari13@gmail.com>
2025-07-11 03:27:33 +0530

    adding notes about loops

---
+++ Day4/day4.js
@@ -0,0 +1,80 @@
+//what is loop?
+/* jo code bar bar repeat hota hai usko loop kehte hai.
+loop ka use tab hota hai jab hume same code bar bar run karna ho
+loop ka use karne se code chhota aur readable hota hai.
+javascript me mainly 3 type ke loop hote hai: */
+
+//=========for loop, while loop, do-while loop==============
+
+
+/*  for loop = for(initialization; condition; increment/decrement){
+        //code to be executed
+        } */
+       
+/*  for(let i=1; i<=10; i++){
+         console.log(i);
+ } */
+       
+ //for loop ka use karke 1 se 20 tak ke saare even number print karna
+/*  for (let k = 1; k <= 20; k++) {
+     if (k % 2 === 0) {
+         console.log(k);
+     }
+ } */
+
+//while use kehte hai jab hume pata ho ki kitne baar loop chalega.
+
+/* //while loop = while(condition){
+        //code to be executed
+         //increment/decrement
+} */   
+
+//while loop ka use karke 10 se 1 tak ke number print karna
+/* let j = 10;
+while (j >= 1) {
+    console.log(j);
+    j--;
+}
+ */
+//while loop ka use karke 1 se 20 tak ke saare odd number print karna
+/* let l = 1;
+while (l <= 20) {
+    if (l % 2 !== 0) {
+        console.log(l);
+    }
+    l++;
+} */
+
+//do-while use kehte hai jab hume pata ho ki loop kam se kam ek baar chalega.
+
+/* do{
+     //code to be executed 
+}while(condition);
+*/
+
+
+//do-while loop ka use karke 1 se 20 tak ke saare odd number print karna
+/* let m = 1;
+do {
+    if (m % 2 !== 0) {
+        console.log(m);
+    }
+    m++;
+}while (m <= 20);  */  
+
+//do-while loop ka use karke 1 se 20 tak ke saare even number print karna
+/* let n = 1;  
+do {
+    if (n % 2 === 0) {
+        console.log(n);
+    }
+    n++;
+}while (n <= 20);   */
+
+//do-while loop ka use karke 1 se 20 tak ke saare number print karna
+/* let o = 1;  
+do {
+    console.log(o);
+    o++;
+}while (o <= 20);
+ */
